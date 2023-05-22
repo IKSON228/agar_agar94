d4431c2b7b81511fa9319dff0c46f19cf4809c3c
IKSON228 <134287370+IKSON228@users.noreply.github.com>
2023-05-22 17:18:00 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from random import shuffle
+spisog_omg = ['соль', 'мазик', 'картоха']
+triks = input('Введи ингридиент (хорош - харе):')
+while triks != 'харе':
+    if triks in spisog_omg
+        print('Этот ингредиент уже есть')
+    else:
+        spisog_omg.append(triks)
+    triks = input('Введи ингридиент (хорош - харе):')
+spisok_blender = []
+namas = int(input('сколько месим ингридиентов:'))
+for i in range(namas):
+    shuffle(spisog_omg)
+    
